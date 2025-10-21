aff401c4ada9bec3120e60205c3e71ce4ad2ae37
Kadir Ayana <33261596+kadirayana@users.noreply.github.com>
2025-10-21 15:19:54 +0300

    Remove unused i18n imports from _app.js
    Removed unused imports for i18n configuration.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-import { appWithTranslation } from 'next-i18next'
-import nextI18NextConfig from '../next-i18next.config.js'
 import React from 'react';
 
 function App({ Component, pageProps }) {
