df9e248bd30ebbc7f27eee14d3cb7e5ddfd98c6b
Kadir Ayana <33261596+kadirayana@users.noreply.github.com>
2025-10-21 16:13:35 +0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>ReactorDesign Migration</title>
         {/* favicon for browser tab (place public/logo.png) */}
-        <link rel="icon" href="/logo.png" />
+        <link rel="icon" href="public/logo.png" />
       </Head>
       <Navbar />
       <main className="main-content" style={{ paddingTop: 16 }}>{children}</main>
