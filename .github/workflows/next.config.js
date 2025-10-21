cb75d6a45736c701249c9ca5c7dcea5bd8d84408
Kadir Ayana <33261596+kadirayana@users.noreply.github.com>
2025-10-21 15:51:09 +0300

    Update next.config.js

--- .github/workflows/next.config.js
+++ .github/workflows/next.config.js
@@ -1,29 +1,18 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  // GitHub Pages için statik HTML'e export etmeyi sağlar
+  reactStrictMode: true,
+
+
   output: 'export',
-  
-  // Build sonrası statik dosyaların çıkarılacağı dizin
+
+
   distDir: 'out',
 
-  // Statik export için resim optimizasyonunu kapatır
   images: {
     unoptimized: true,
   },
 
-  // React'in potansiyel problemleri belirlemesine yardımcı olur
-  reactStrictMode: true,
-
-  // URL'lerin sonuna '/' ekler (ör: /about/)
-  trailingSlash: true,
-
-  // Projenin alt yolunu belirler (ör: username.github.io/ReactorDesign)
   basePath: '/ReactorDesign',
-
-  // Varlıkların (JS, CSS, resimler) yükleneceği yolu belirler
   assetPrefix: '/ReactorDesign/',
 };
 
 module.exports = nextConfig;
-
